1ee662d6a452322c98ee654917eb496d8e9b580a
muneeb.ahmed <muneeb.ahmed@amigo-software.com>
2025-05-02 17:04:26 +0500

    Board error code done

--- test_Provisioning/test_36_Board.py
+++ test_Provisioning/test_36_Board.py
@@ -16,6 +16,7 @@ import test_Provisioning.test_01_SystemSettings as systemsettingsFunctions
 from test_Provisioning import test_37_Equipment as EquipmentFun
 from test_Provisioning import test_03_PacketizerConfiguration as PacketizerFun
 from Key import config
+from Key_User import config as boardUser
 
 SheetName=	'36-Board'
 
@@ -816,4 +817,310 @@ class test_4_DeleteBoard(TestCase):
                     common.UpdateExcelTestCase(SheetName, TestCaseID, URL, Parameters, status, starttime, resp)
         else:
                 TestCasesStatus=False
+    # Test Case End
+
+# This class cases added by muneeb.ahmed in release 2.5.0.17
+class test_5_BoardErrorCodes(TestCase):
+
+    # Start Test Case No 36-21
+    def testcase_01_AddBoard(self, TestCasesStatus=True):
+
+        TestCaseID = '36-21'
+        # Calling Common Functions
+        common = CF.CommonFunctions()
+        common.Header('Board', 'Using Post Method to Add Board', 'Add Board When Board name character more then 250.')
+        # System Settings Configuration Functions Calling
+        SystemSettingsFunction = systemsettingsFunctions.test_1_UpdateSystemSettings()
+        SystemSettingsFunction.testcase_01_UpdateSystemSettings(common.PrereqTestCasesStatusUpdate)
+
+        # Generate Simple Character String Limit 10 Characters
+        Name = common.GenrateDesc250()
+        LogicalName = common.GenrateSimpleStringLimit10()
+        # Test Case Start Time
+        starttime = time.process_time()
+        # Header Parameters of Rest API
+        Parameters = {'AuthToken': config.sessionkey,
+                      'AuthUser': config.auth_user,
+                      'Name': '' + Name + '',
+                      'LogicalName': '' + LogicalName + '',
+
+                      }
+
+        # Url For Add Board
+        UrlforAddBoard = '/Board/Add'
+        URL = '' + common.Domain + '' + UrlforAddBoard + ''
+        # Hit API Through Methods
+        response = requests.post(URL, headers=Parameters)
+        # API Response in JSon Format
+        resp = response.json()
+        showcode = str(resp['ResponseCode'])
+
+        # Response Code Verification
+        if TestCasesStatus == True:
+            try:
+                if resp['ResponseCode'] == 400 and resp['InternalErrorCode'] == 'OPRBd06':
+                    print(common.SuccessMessage)
+                    status = 'Passed'
+                else:
+                    status = 'Failed'
+                    assert False
+
+            # Write Output Result in Excel File
+            finally:
+                common.UpdateExcelTestCase(SheetName, TestCaseID, URL, Parameters, status, starttime, resp)
+        else:
+            TestCasesStatus = False
+            # Test Case End
+
+        # Start Test Case No 36-01
+
+    # Start Test Case No 36-22
+    def testcase_02_AddBoard(self, TestCasesStatus=True):
+
+        TestCaseID = '36-22'
+        # Calling Common Functions
+        common = CF.CommonFunctions()
+        common.Header('Board', 'Using Post Method to Add Board', 'Add Board with all valid Data.')
+        # System Settings Configuration Functions Calling
+        SystemSettingsFunction = systemsettingsFunctions.test_1_UpdateSystemSettings()
+        SystemSettingsFunction.testcase_01_UpdateSystemSettings(common.PrereqTestCasesStatusUpdate)
+
+        # Generate Simple Character String Limit 10 Characters
+        Name = common.GenrateSimpleStringLimit10()
+        LogicalName = common.GenrateDesc250()
+        # Test Case Start Time
+        starttime = time.process_time()
+        # Header Parameters of Rest API
+        Parameters = {'AuthToken': config.sessionkey,
+                      'AuthUser': config.auth_user,
+                      'Name': '' + Name + '',
+                      'LogicalName': '' + LogicalName + '',
+
+                      }
+
+        # Url For Add Board
+        UrlforAddBoard = '/Board/Add'
+        URL = '' + common.Domain + '' + UrlforAddBoard + ''
+        # Hit API Through Methods
+        response = requests.post(URL, headers=Parameters)
+        # API Response in JSon Format
+        resp = response.json()
+        showcode = str(resp['ResponseCode'])
+
+        # Response Code Verification
+        if TestCasesStatus == True:
+            try:
+                if resp['ResponseCode'] == 400 and resp['InternalErrorCode'] == 'OPRBd07':
+                    print(common.SuccessMessage)
+                    status = 'Passed'
+                else:
+                    status = 'Failed'
+                    assert False
+
+            # Write Output Result in Excel File
+            finally:
+                common.UpdateExcelTestCase(SheetName, TestCaseID, URL, Parameters, status, starttime, resp)
+        else:
+            TestCasesStatus = False
+
+        return Name
+    # Test Case End
+
+    # Start Test Case No 36-23
+    def testcase_03_UpdateBoard(self, TestCasesStatus=True):
+
+            TestCaseID = '36-33'
+            # Calling Common Functions
+            common = CF.CommonFunctions()
+            common.Header('Board', 'Using Get Method to Get Board data',
+                          'Get a single Board data with invalid/Non Existing ID.')
+
+            BoardID = '123455'
+            # Test Case Start Time
+            starttime = time.process_time()
+            # Header Parameters of Rest API
+            Parameters = {'AuthToken': config.sessionkey,
+                          'AuthUser': config.auth_user,
+                          'ID': '' + BoardID + '',
+
+                          }
+
+            # Url For Get Board
+            UrlforGetBoard = '/Board/Get/'
+            URL = '' + common.Domain + '' + UrlforGetBoard + '' + BoardID + ''
+            # Hit API Through Methods
+            response = requests.get(URL, headers=Parameters)
+            # API Response in JSon Format
+            resp = response.json()
+            showcode = str(resp['ResponseCode'])
+
+            # Response Code Verification
+            if TestCasesStatus == True:
+                try:
+                    if resp['ResponseCode'] == 400 and resp['InternalErrorCode'] == 'OPRBd08':
+                        print(common.SuccessMessage)
+                        status = 'Passed'
+                    else:
+                        status = 'Failed'
+                        assert False
+
+                # Write Output Result in Excel File
+                finally:
+                    common.UpdateExcelTestCase(SheetName, TestCaseID, URL, Parameters, status, starttime, resp)
+            else:
+                TestCasesStatus = False
+    # Test Case End
+
+    # Start Test Case No 36-24
+    def testcase_04_DeleteBoard(self, TestCasesStatus=True):
+
+        TestCaseID = '36-24'
+        # Calling Common Functions
+        common = CF.CommonFunctions()
+        common.Header('Board', 'Using Delete Method to Delete Board data',
+                      'Delete a single Board data with valid ID that is associated with equipment.')
+
+        # Add Equipment Function calling
+        EquipmentFunctions = EquipmentFun.test_1_AddEquipment()
+        EquipmentNumber, BoardID, TrunkGroupID = EquipmentFunctions.testcase_01_AddEquipment(
+            common.PrereqTestCasesStatusUpdate)
+        TrunkGroupID = TrunkGroupID
+        EquipmentNumber = EquipmentNumber
+
+        # Test Case Start Time
+        starttime = time.process_time()
+        # Header Parameters of Rest API
+        Parameters = {'AuthToken': config.sessionkey,
+                      'AuthUser': config.auth_user,
+                      'ID': '' + BoardID + '',
+
+                      }
+
+        # Url For Delete Board
+        UrlforDeleteBoard = '/Board/Delete/'
+        URL = '' + common.Domain + '' + UrlforDeleteBoard + '' + BoardID + ''
+        # Hit API Through Methods
+        response = requests.delete(URL, headers=Parameters)
+        # API Response in JSon Format
+        resp = response.json()
+        showcode = str(resp['ResponseCode'])
+
+        # Response Code Verification
+        if TestCasesStatus == True:
+            try:
+                if resp['ResponseCode'] == 409 and resp['InternalErrorCode'] == 'OPRBd11':
+                    print(common.SuccessMessage)
+                    status = 'Passed'
+                else:
+                    status = 'Failed'
+                    assert False
+
+            # Write Output Result in Excel File
+            finally:
+                common.UpdateExcelTestCase(SheetName, TestCaseID, URL, Parameters, status, starttime, resp)
+        else:
+            TestCasesStatus = False
+    # Test Case End
+
+    # Start Test Case No 36-25
+    def testcase_05_DeleteBoard(self, TestCasesStatus=True):
+
+            TestCaseID = '36-25'
+            # Calling Common Functions
+            common = CF.CommonFunctions()
+            common.Header('Board', 'Using Delete Method to Delete Board data',
+                          'Delete a single Board data with valid ID that is associated with Packetizer.')
+
+            # Add Packetizer Function calling
+            PacketizerFunctions = PacketizerFun.test_1_AddPacketizerConfiguration()
+            PcktIP, BoardID, TrunkGroupID = PacketizerFunctions.testcase_22_AddPacketizerConfiguration(
+                common.PrereqTestCasesStatusUpdate)
+            PcktIP = PcktIP
+            TrunkGroupID = TrunkGroupID
+
+            # Test Case Start Time
+            starttime = time.process_time()
+            # Header Parameters of Rest API
+            Parameters = {'AuthToken': config.sessionkey,
+                          'AuthUser': config.auth_user,
+                          'ID': '' + BoardID + '',
+
+                          }
+
+            # Url For Delete Board
+            UrlforDeleteBoard = '/Board/Delete/'
+            URL = '' + common.Domain + '' + UrlforDeleteBoard + '' + BoardID + ''
+            # Hit API Through Methods
+            response = requests.delete(URL, headers=Parameters)
+            # API Response in JSon Format
+            resp = response.json()
+            showcode = str(resp['ResponseCode'])
+
+            # Response Code Verification
+            if TestCasesStatus == True:
+                try:
+                    if resp['ResponseCode'] == 409 and resp['InternalErrorCode'] == 'OPRBd14':
+                        print(common.SuccessMessage)
+                        status = 'Passed'
+                    else:
+                        status = 'Failed'
+                        assert False
+
+                # Write Output Result in Excel File
+                finally:
+                    common.UpdateExcelTestCase(SheetName, TestCaseID, URL, Parameters, status, starttime, resp)
+            else:
+                TestCasesStatus = False
+    # Test Case End
+
+    # Start Test Case No 36-26
+    def testcase_06_AddBoard(self, TestCasesStatus=True):
+
+        TestCaseID = '36-26'
+        # Calling Common Functions
+        common = CF.CommonFunctions()
+        common.Header('Board', 'Using Post Method to Add Board', 'Add Board with all valid Data. Without user rights to board')
+        # System Settings Configuration Functions Calling
+        SystemSettingsFunction = systemsettingsFunctions.test_1_UpdateSystemSettings()
+        SystemSettingsFunction.testcase_01_UpdateSystemSettings(common.PrereqTestCasesStatusUpdate)
+
+        # Generate Simple Character String Limit 10 Characters
+        Name = common.GenrateSimpleStringLimit10()
+        LogicalName = common.GenrateSimpleStringLimit10()
+        # Test Case Start Time
+        starttime = time.process_time()
+        # Header Parameters of Rest API
+        Parameters = {'AuthToken': boardUser.sessionkey,
+                      'AuthUser': boardUser.auth_user,
+                      'Name': '' + Name + '',
+                      'LogicalName': '' + LogicalName + '',
+
+                      }
+
+        # Url For Add Board
+        UrlforAddBoard = '/Board/Add'
+        URL = '' + common.Domain + '' + UrlforAddBoard + ''
+        # Hit API Through Methods
+        response = requests.post(URL, headers=Parameters)
+        # API Response in JSon Format
+        resp = response.json()
+        showcode = str(resp['ResponseCode'])
+
+        # Response Code Verification
+        if TestCasesStatus == True:
+            try:
+                if resp['ResponseCode'] == 403 and resp['InternalErrorCode'] == 'OPRBd12':
+                    print(common.SuccessMessage)
+                    status = 'Passed'
+                else:
+                    status = 'Failed'
+                    assert False
+
+            # Write Output Result in Excel File
+            finally:
+                common.UpdateExcelTestCase(SheetName, TestCaseID, URL, Parameters, status, starttime, resp)
+        else:
+            TestCasesStatus = False
+
+        return Name
     # Test Case End
